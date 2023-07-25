9bc9181653c76534ab9bbea8a534f1e06d6f6605
GOKUL R <88566558+GokulR83@users.noreply.github.com>
2023-07-25 11:27:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function black(){
+    var temp=document.getElementById('change');
+    temp.src="./car/grey.jpg";
+}
+function white(){
+   var tem= document.getElementById('change');
+    tem.src="./car/white.webp";
+}
+function red(){
+    var temp=document.getElementById('change');
+    temp.src="./car/red.jpg";
+}
+function result(){
+    var sele=document.getElementById('buy');
+    sele.innerText="Order Placed";
+}
